9003651dcf911355041798c682a5bd0f27cb8087
Brian <brianwwe2k15@gmail.com>
2025-04-26 11:51:05 -0300

    primeiro commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'], // ou seus caminhos reais
+    theme: {
+      extend: {
+        fontFamily: {
+          bebas: ['"Bebas Neue"', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  }
+  
